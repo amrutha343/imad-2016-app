04ff1beb25161c3570a266af9b3aadebf78bb1e3
amrutha343 <amrutha343@gmail.com>
2016-09-22 20:28:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var element=document.getElementById('maintext');
 element.innerHTML='new value';
 
-var img=document.getElementById('madi');
+var img=document.getElementById("madi");
 img.onClick=function()
 {
   img.style.marginLeft='100px';  
