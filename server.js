e4677a3b26097acb4823316f637fd3934acad431
amrutha343 <amrutha343@gmail.com>
2016-09-24 19:21:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,6 +100,7 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 var counter=0;
 app.get('/counter', function (req, res) {
    counter=counter+1;
@@ -110,8 +111,6 @@ app.get('/:articleName', function(req,res){
 var articleName=req.params.articleName;
 res.send(createTemplate(articles[articleName]));
 });
-
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
