66c936c19d0f5599780945ffb3b79454e04e46e4
amrutha343 <amrutha343@gmail.com>
2016-09-25 14:54:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,19 @@
-console.log('Loadded!');
-
-
+var button=document.getElementById('counter');
+button.onclick()=function ()
+{
+    var request= new XMLHttpRequest();
+    request.onreadystatechange=function ()
+    {
+        if(request.readystate===XMLHttpRequest.DONE)
+        {
+            if(request.status===200)
+            {
+                var counter=request.responsetext;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+    request.open('GET','https://amrutha343.imad.hasura-app.io/counter',true);
+    request.send(NULL);
+};
